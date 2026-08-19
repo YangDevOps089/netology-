6c6f06b43787f09011642dc84b2a909c3d615fae
YangDevOps089 <gracevandrej089@gmail.com>
2026-08-19 22:48:43 +1100

    hw1.2: mysql container via terraform remote docker context (ssh)

---
+++ ter-homeworks/01/1.2/extra/db.tf
@@ -0,0 +1,49 @@
+provider "docker" {
+  host = "ssh://devops@158.160.84.74:22"
+}
+
+terraform {
+  required_providers {
+    
+    docker = {
+        source  = "kreuzwerker/docker"
+        version = "~> 3.0"
+    }
+    random = {
+        source = "hashicorp/random"
+        version = "~> 3.5"
+        }
+    }
+}
+
+resource "random_password" "root_pass" {
+    length = 16
+    special = false
+}
+
+resource "random_password" "user_pass" {
+    length = 16
+    special = false
+}
+
+resource "docker_image" "mysql" {
+    name = "mysql:8"
+}
+
+resource "docker_container" "mysql" {
+    name  = "example_${random_password.root_pass.result}"
+    image = docker_image.mysql.image_id
+
+    ports {
+      internal = 3306
+      external = 3306
+      ip       = "127.0.0.1"
+    }
+      env = [
+    "MYSQL_ROOT_PASSWORD=${random_password.root_pass.result}",
+    "MYSQL_DATABASE=wordpress",
+    "MYSQL_USER=wordpress",
+    "MYSQL_PASSWORD=${random_password.user_pass.result}",
+    "MYSQL_ROOT_HOST=%",
+  ]
+}
